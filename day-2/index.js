76bdbb90de0be92925303b772fe8b99edf095ad0
Adam Thometz <adamthometz@gmail.com>
2022-12-07 16:24:09 -0500

    completed day 2

---
+++ day-2/index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('\n');
+
+const keyToPlay = {
+  // opponent
+  A: 'rock',
+  B: 'paper',
+  C: 'scissors',
+  // Part 1
+  // self
+  // X: 'rock',
+  // Y: 'paper',
+  // Z: 'scissors',
+  // Part 2
+  X: 'lose',
+  Y: 'draw',
+  Z: 'win'
+}
+
+// key beats value
+const rules = {
+  rock: {
+    beats: 'scissors',
+    losesTo: 'paper',
+    value: 1
+  },
+  paper: {
+    beats: 'rock',
+    losesTo: 'scissors',
+    value: 2
+  },
+  scissors: {
+    beats: 'paper',
+    losesTo: 'rock',
+    value: 3
+  }
+};
+
+let score = 0;
+
+for (let round of input) {
+  // Initialize total
+  let total = 0;
+
+  // Get info about plays
+  const [opponent, self] = round.split(' ');
+  const opponentPlay = keyToPlay[opponent];
+  // selfPlay in Part 1
+  const plan = keyToPlay[self];
+
+  // Add the value of your play off the bat
+  // Part 1
+  // total += rules[selfPlay].value;
+  // Part 2
+  let selfPlay;
+  if (plan === 'win') {
+    selfPlay = rules[opponentPlay].losesTo;
+  } else if (plan === 'lose') {
+    selfPlay = rules[opponentPlay].beats;
+  } else {
+    selfPlay = opponentPlay;
+  }
+  total += rules[selfPlay].value;
+  
+  // Check if draw
+  if (opponentPlay === selfPlay) total += 3;
+  
+  // Check if you win
+  if (rules[selfPlay].beats === opponentPlay) total += 6;
+
+  score += total;
+}
+
+console.log(score)
